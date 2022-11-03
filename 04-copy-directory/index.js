78ba862ae42a22cb0ebaf968269ca3c9a7aa00cf
Vladislav <serov.vladsilaw@yandex.ru>
2022-11-03 18:31:07 +0300

    fix: deleting files before copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,20 +5,23 @@ const path = require('path');
 const copyDirPath = path.resolve(__dirname, 'files-copy');
 const pathDir = path.resolve(__dirname, 'files');
 
+
 fs.mkdir(copyDirPath, { recursive: true }, (err) => {
     if (err) throw err;
 });
 async function copyFiles() {
+    const copyfiles = await readdir(copyDirPath);
+    for (const file of copyfiles) {
+        let fileCopy = path.resolve(copyDirPath, file)
+        fs.unlink(fileCopy, err => {
+            if(err) throw err; // не удалось удалить файл
+        });
+    }
     const files = await readdir(pathDir);
     for (const file of files) {
-        try {
-            let fileCopy = path.resolve(copyDirPath, file)
-            let originalFile = path.resolve(pathDir, file)
-            await copyFile(originalFile, fileCopy);
-            console.log(`${file} was copied`);
-        } catch {
-            console.log('The file could not be copied');
-        }
+        let fileCopy = path.resolve(copyDirPath, file)
+        let originalFile = path.resolve(pathDir, file)
+        await copyFile(originalFile, fileCopy);
     }
     
 }
